cfebf49c840e7b0216f74a1386cd6a22992cc1bd
lhw <739619784@qq.com>
2017-08-01 09:54:51 +0800

    完善了手机端的滑动分页

--- 移动端分页工具/pagging.js
+++ 移动端分页工具/pagging.js
@@ -1,4 +1,4 @@
-var template = '<p>你好</p>';
+
 (function($){
   var pageHeight;
   var scrollHeight;
@@ -7,30 +7,44 @@ var template = '<p>你好</p>';
   var $ulEle; //ul 增加 dom 的容器
 
 
-  var Pagging = function(){
+  var Pagging = function(obj){
     
     //判断是用函数创建的还是用new创建的。这样我们就可以通过MaskShare("dom") 或 new MaskShare("dom")来使用这个插件了
     if(!(this instanceof Pagging)){
-        return new Pagging();
+      //  var defaultObj = {
+      //       template: '<p>你好</p>',
+      //       el: '.container ul',
+      //       refreshLength: 50, //距离底部多高刷新页面
+      //     }
+        return new Pagging(obj);
     }
 
       //初始化
-      this._init();
+      this._init(obj);
   }
   
   Pagging.prototype = {
-    _init: function(){
+    _init: function(obj){
      
+      this.extend(obj)
       //初始化数据
-      this.initData();
+      this.initData(obj);
       //绑定事件
-      this.bindEvent();
+      this.bindEvent(this.dataObj);
     },
-    initData: function(){
-       refresh = 50;
-      $ulEle = $('.container ul');
+    initData: function(obj){
+      
+      refresh = obj.refreshLength;
     },
-    bindEvent: function(){
+    extend: function(obj){
+      var dataObj = {};
+      for(key in obj){
+        dataObj[key] = obj[key]
+      }
+      this.dataObj = dataObj;
+    },
+    bindEvent: function(obj){
+      var that = this;
       $('.container').on('touchstart',function(){
         pageHeight = $(window).height();
         scrollHeight = $(document).height();
@@ -41,14 +55,18 @@ var template = '<p>你好</p>';
         if(scrollHeight > pageHeight){
           if(scrollHeight - scrollTop - pageHeight < refresh){
             //加载数据
-            console.log('move')
-            $ulEle.append(template);
+            that.drawPage(obj)
           }
         }
       });
       $('.container').on('touchend',function(){
           console.log('end')
       });
+    },
+    drawPage: function(obj){
+      console.log(JSON.stringify(obj))
+      var $dom = $(obj.el);
+      $dom.append(obj.template);
     }
   }
   //暴露全局变量
